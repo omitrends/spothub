8a53ba44eae047b821f47176c49e0d765a2412e9
omitrends <omkardeshmukh721072@gmail.com>
2025-09-11 14:50:51 +0530

    login files changes

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -123,7 +123,7 @@ function Login() {
             <div className="mt-6 grid grid-cols-1 gap-3">
               {[
                 {
-                  href: "http://localhost:4000/api/auth/google",
+                  href: "https://spothub.onrender.com/api/auth/google",
                   img: "/assets/google.svg",
                   text: "Google",
                 }
